cd864a0f470264c1d95378c3c683962e836b5a72
김대석 <greatstone@satreci.com>
2016-06-12 01:14:38 +0900

    페이스북 공유 기능 추가

--- iOS/siphabet/siphabet/ViewController.swift
+++ iOS/siphabet/siphabet/ViewController.swift
@@ -7,14 +7,29 @@
 //
 
 import UIKit
+import Social
 
 class ViewController: UIViewController {
     
+    let imageUrl = NSURL(string:"https://i.guim.co.uk/img/static/sys-images/Guardian/Pix/pictures/2014/11/14/1415979995987/c7dbed8a-6f29-4afe-90d8-503d5093dd6d-1020x574.jpeg?w=720&q=55&auto=format&usm=12&fit=max&s=351f0363b5c89faccbf76ca987b3ef17")
     @IBOutlet var name: UITextField!
     @IBOutlet var result: UILabel!
+    @IBOutlet var facebookShare: UIButton!
+    @IBOutlet var imageView: UIImageView!
     
     @IBAction func clickOk() {
         result.text = name.text
+        facebookShare.hidden = false
+        
+        let data = NSData(contentsOfURL: imageUrl!)
+        imageView.image = UIImage(data: data!)
     }
     
+    @IBAction func clickFacebookShare() {
+        let composeSheet = SLComposeViewController(forServiceType: SLServiceTypeFacebook)
+        composeSheet.setInitialText("Share satellite images for your name!!")
+        composeSheet.addURL(imageUrl)
+        
+        presentViewController(composeSheet, animated: true, completion: nil)
+    }
 }
