f3aed4d101150fab280df604a47d5beb463a31b3
김대석 <greatstone@satreci.com>
2016-06-29 17:55:25 +0900

    기능 화면 분리, 레이아웃 정렬, 로딩 화면 추가

--- iOS/siphabet/siphabet/ResultViewController.swift
+++ iOS/siphabet/siphabet/ResultViewController.swift
@@ -1,25 +1,23 @@
 //
-//  ViewController.swift
+//  ResultViewController.swift
 //  siphabet
 //
-//  Created by 김대석 on 2016. 6. 11..
+//  Created by 김대석 on 2016. 6. 28..
 //  Copyright © 2016년 김대석. All rights reserved.
 //
 
 import UIKit
 import Social
 
-class ViewController: UIViewController {
+class ResultViewController: UIViewController {
+    
+    var name: String!
     
     let imageUrl = NSURL(string:"https://i.guim.co.uk/img/static/sys-images/Guardian/Pix/pictures/2014/11/14/1415979995987/c7dbed8a-6f29-4afe-90d8-503d5093dd6d-1020x574.jpeg?w=720&q=55&auto=format&usm=12&fit=max&s=351f0363b5c89faccbf76ca987b3ef17")
-    @IBOutlet var name: UITextField!
-    @IBOutlet var result: UILabel!
-    @IBOutlet var facebookShare: UIButton!
     @IBOutlet var imageView: UIImageView!
     
-    @IBAction func clickOk() {
-        result.text = name.text
-        facebookShare.hidden = false
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
         let data = NSData(contentsOfURL: imageUrl!)
         imageView.image = UIImage(data: data!)
