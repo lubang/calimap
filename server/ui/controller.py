5fec70d788164ef6a535a957d68ef823e13ba889
lubang <lubang@lulab.net>
2016-07-27 00:52:37 +0900

    Implement to serve map image alphabet.

--- server/ui/controller.py
+++ server/ui/controller.py
@@ -3,7 +3,7 @@ from os.path import dirname, join
 
 blueprint = Blueprint('controller', __name__)
 rootDir = join(dirname(dirname(__file__)), 'web')
-
+dbDir = join(dirname(dirname(__file__)), 'db')
 
 @blueprint.route('/')
 def root():
@@ -13,3 +13,8 @@ def root():
 @blueprint.route('/<path:filename>')
 def serve(filename):
     return send_from_directory(rootDir, filename)
+
+
+@blueprint.route('/db/<path:filename>')
+def serve_image(filename):
+    return send_from_directory(dbDir, filename)
