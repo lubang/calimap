641eb97da58fde46d5561fd9c58a6f2497e949d7
석정철 <jcseok@satreci.com>
2016-07-26 20:56:00 +0900

    data #1

--- server/db/seed/init.sql
+++ server/db/seed/init.sql
@@ -29,3 +29,46 @@ values ('D', 5.0, 5.1, 6, "DDD");
 
 insert into alphabet (alphabet, lat, long, zoom, image_path)
 values ('E', 7.0, 7.1, 8, "EEE"); 
+
+insert into alphabet (alphabet, lat, long, zoom, image_path)
+values ('s', -36.00101, 144.42777, 8, "db/mapImage/s/lower/s_1.PNG");
+
+insert into alphabet (alphabet, lat, long, zoom, image_path)
+values ('o', -37.819937, 144.983224, 8, "db/mapImage/o/lower/o_1.PNG");
+
+insert into alphabet (alphabet, lat, long, zoom, image_path)
+values ('A', -37.823337, 144.974673, 8, "db/mapImage/a/upper/a_1.PNG");
+
+insert into alphabet (alphabet, lat, long, zoom, image_path)
+values ('A', -36.889918 145.18012, 8, "db/mapImage/a/upper/a_2.PNG");
+
+insert into alphabet (alphabet, lat, long, zoom, image_path)
+values ('c', 25.844421, 50.621164, 8, "db/mapImage/c/lower/c_1.PNG");
+
+insert into alphabet (alphabet, lat, long, zoom, image_path)
+values ('c', -19.394921, -138.675967, 8, "db/mapImage/c/lower/c_2.PNG");
+
+insert into alphabet (alphabet, lat, long, zoom, image_path)
+values ('D', 53.010841, -81.329146, 8, "db/mapImage/d/upper/d_1.PNG");
+
+insert into alphabet (alphabet, lat, long, zoom, image_path)
+values ('i', 12.030647, 92.675341, 8, "db/mapImage/i/lower/i_1.PNG");
+
+insert into alphabet (alphabet, lat, long, zoom, image_path)
+values ('j', -18.357107, 146.818856, 8, "db/mapImage/j/lower/j_1.PNG");
+
+insert into alphabet (alphabet, lat, long, zoom, image_path)
+values ('o', 22.918999, -10.406858, 8, "db/mapImage/o/lower/o_2.PNG");
+
+insert into alphabet (alphabet, lat, long, zoom, image_path)
+values ('p', 60.399733, -112.456873, 8, "db/mapImage/p/lower/p_1.PNG");
+
+insert into alphabet (alphabet, lat, long, zoom, image_path)
+values ('r', -42.689838, -71.897315, 8, "db/mapImage/r/lower/r_1.PNG");
+
+insert into alphabet (alphabet, lat, long, zoom, image_path)
+values ('u', 37.152181, -109.881485, 8, "db/mapImage/u/lower/u_1.PNG");
+
+
+
+
