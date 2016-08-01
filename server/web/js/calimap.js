0ffa79d079deaf8cf387206ecde77361a287b44d
shlee <shlee@satreic.om>
2016-08-01 23:04:08 +0900

    [Web] 검색 시 ajax 요청, 응답

--- server/web/js/calimap.js
+++ server/web/js/calimap.js
@@ -1,19 +1,22 @@
-function getAlphabet(){
-	var input = document.getElementById('inputAlphabet').value;
-
-	$.ajax({
-		method: "GET",
-		dataType: "json",
-		url: "/api/sipabet",
-		contentType: "application/json; charset=UTF-8",
-		data: JSON.stringify({
-			"input" : input
-		})
-	})
-	.done(function(){
-		alert("success");
-	})
-	.fail(function(){
-		alert("fail");
-	})
+function getAlphabet() {
+    var input = document.getElementById('inputAlphabet').value;
+    var request = $.ajax({
+        url: 'http://calimap.party/api/v1/alphabet/text=' + input
+        , method: 'GET'
+        , headers: {
+            'Access-Control-Allow-Origin': '*'
+        }
+        , dataType: 'JSON'
+        , success: function (response) {
+            console.log(response);
+            var img = response.cali[0].image;
+                document.getElementById('outputDiv').innerHTML = "<img src=\"\">";
+            for(var i=0; i<response.cali.length; i++){
+                document.getElementById('outputDiv').innerHTML += "<img style=\"width:300px;height:300px\"src=\"http://calimap.party/" + response.cali[i].image + "\"/>";
+            }
+            
+        }
+    }).fail(function(){
+        alert("올바른 입력을 해주세요");
+    });
 }
